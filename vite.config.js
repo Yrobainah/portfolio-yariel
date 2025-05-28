ce2d4cd0fc04b1f6bd91e2d2c99df72f3ecbf25f
Yrobainah <69661560+Yrobainah@users.noreply.github.com>
2025-05-28 13:17:21 +0200

    modificaciones de configuración y agregar carpeta de screenshots

--- vite.config.js
+++ vite.config.js
@@ -4,20 +4,22 @@ import tailwindcss from '@tailwindcss/vite'
 
 // https://vite.dev/config/
 export default defineConfig({
-  plugins: [vue(),
-  tailwindcss({
-    config: {
-      darkMode: 'class',
-      content: [
-        './index.html',
-        './src/**/*.{vue,js,ts,jsx,tsx}',
-      ],
-      theme: {
-        extend: {},
+  base: '/portfolio-yariel/',
+  plugins: [
+    vue(),
+    tailwindcss({
+      config: {
+        darkMode: 'class',
+        content: [
+          './index.html',
+          './src/**/*.{vue,js,ts,jsx,tsx}',
+        ],
+        theme: {
+          extend: {},
+        },
+        plugins: [],
       },
-      plugins: [],
-    },
-  }),
+    }),
   ],
 })
 
